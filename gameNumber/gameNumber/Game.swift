32dc2455c8ace6a6dc7eedc464a16372417409a7
VladislavV1P <98524936+VladislavV1P@users.noreply.github.com>
2022-02-17 00:28:46 +0300

    addRefreshButton

--- gameNumber/gameNumber/Game.swift
+++ gameNumber/gameNumber/Game.swift
@@ -10,6 +10,7 @@ import Foundation
 enum StatusGame {
     case start
     case win
+    case lose
 }
 
 class Game {
@@ -17,6 +18,7 @@ class Game {
     struct Item {
         var titel: String
         var isFound: Bool = false
+        var isErrors = false
     }
     
     
@@ -24,31 +26,78 @@ class Game {
     var items: [Item] = []
     private var countItems: Int
     var nextItem: Item?
-    var status: StatusGame = .start
     
-    init(countItems: Int) {
+    var status: StatusGame = .start {
+        didSet {
+            if status != .start {
+                stopGame()
+            }
+        }
+    }
+
+    private var timeForGame: Int {
+        didSet {
+            if timeForGame == 0 {
+                status = .lose
+            }
+            updateTimer(status, timeForGame )
+        }
+    }
+    private var startTime: Int
+    
+    private var timer: Timer?
+    
+    private var updateTimer:((StatusGame,Int) -> Void)
+    
+    init(countItems: Int, time: Int, updateTimer:@escaping (_ status: StatusGame, _ second: Int) -> Void) {
         self.countItems = countItems
+        self.timeForGame = time
+        self.startTime = time
+        self.updateTimer = updateTimer
         setupGame()
     }
      
     private func setupGame() {
+        items.removeAll()
         var digits = data.shuffled()
         
         while items.count < countItems {
             let item = Item(titel: String(digits.removeFirst()))
             items.append(item)
         }
+         
         nextItem = items.shuffled().first
-    }
-    func check(index: Int) {
         
+        updateTimer(status, timeForGame)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self]
+            (_) in
+            self?.timeForGame -= 1
+        })
+    }
+    
+    func newGame() {
+        status = .start
+        self.timeForGame = self.startTime
+        setupGame()
+    }
+    
+    
+    func check(index: Int) {
+        guard status == .start else {return }
         if items[index].titel == nextItem?.titel  {
                 items[index].isFound = true
             nextItem = items.shuffled().first(where: {$0.isFound == false})
             
+        } else {
+            items[index].isErrors  = true
         }
         if nextItem == nil {
             status = .win
         }
+    }
+    
+    private func stopGame() {
+        timer?.invalidate()
     }
 }
--- gameNumber/gameNumber/GameViewController.swift
+++ gameNumber/gameNumber/GameViewController.swift
@@ -13,10 +13,18 @@ class GameViewController: UIViewController {
 
     @IBOutlet weak var statusGameLabel: UILabel!
     @IBOutlet weak var  nextDigit: UILabel!
+    @IBOutlet weak var tamerLabel: UILabel!
     
     
-    lazy var game = Game(countItems: buttonCollection.count)
+    @IBOutlet weak var refreshButton: UIButton!
     
+    
+    lazy var game = Game(countItems: buttonCollection.count, time:  30) { [weak self] (status, time) in
+        guard let self =  self else {return}
+        self.tamerLabel.text = time.secondsToString()
+        self.updateStatusGame(status: status)
+    }
+     
     override func viewDidLoad() {
         super.viewDidLoad()
          
@@ -25,6 +33,12 @@ class GameViewController: UIViewController {
     }
     
 
+    @IBAction func refreshButtonNew(_ sender: UIButton) {
+        game.newGame()
+        setupScreen()
+
+    }
+    
     @IBAction func pressButton(_ sender: UIButton) {
         guard let buttonIndex = buttonCollection.firstIndex(of: sender) else {
             return
@@ -34,6 +48,7 @@ class GameViewController: UIViewController {
     }
     
     private func setupScreen() {
+        refreshButton.isHidden = true
         for index in game.items.indices {
             buttonCollection[index].setTitle(game.items[index].titel, for: .normal)
             buttonCollection[index].isHidden = false
@@ -43,11 +58,43 @@ class GameViewController: UIViewController {
     private func updateIU() {
         for index in game.items.indices {
             buttonCollection[index].isHidden = game.items[index].isFound
+            if game.items[index].isErrors {
+                UIView.animate(withDuration: 0.3) { [weak self] in
+                    self?.buttonCollection[index].backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+                } completion: { [weak self] (_) in
+                    self?.buttonCollection[index].backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                    self?.game.items[index].isErrors = false
+                }
+
+            }
         }
         nextDigit.text = game.nextItem?.titel
-        if game.status == .win {
-            statusGameLabel.text = "Вы победили"
+        
+        updateStatusGame(status: game.status)
+    }
+    
+    private func updateStatusGame(status: StatusGame) {
+        switch status {
+        case .start:
+            statusGameLabel.text = "Игра началась"
+            statusGameLabel.textColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        case  .win:
+            statusGameLabel.text = "Вы выиграли"
+            statusGameLabel.textColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+            refreshButton.isHidden = false
+        case .lose:
+            statusGameLabel.text = "Вы проиграли"
+            statusGameLabel.textColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+            refreshButton.isHidden = false
         }
     }
 
+}
+
+extension Int {
+    func secondsToString() -> String {
+        let minute = self / 60
+        let second = self % 60
+        return String(format: "%d:%02d", minute,second )
+    }
 }
